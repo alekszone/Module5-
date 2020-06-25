2fdc6a7245c4cae4178a1172dac114513286cc70
alekszone <50597224+alekszone@users.noreply.github.com>
2020-06-25 14:27:36 +0200

    up

--- day3/src/users/user/index.js
+++ day3/src/users/user/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const path = require("path")
 const fs = require("fs")
-// const uniqId = require("uniqid")
+const uniqId = require("uniqid")
 const { request } = require("http")
 const { response } = require("express")
 const { param } = require("express/lib/router")
@@ -14,6 +14,7 @@ const moveData = fs.readFileSync(usersData)
 const users = JSON.parse(moveData.toString())
 return users
 }
+const usersData = path.join(__dirname,"users.json")
 
 router.get("/",(request,response)=>{
 const allUsers = getUsers()
@@ -23,18 +24,58 @@ else
 response.send("No users")
 })
 
-router.get("/:id",(request,response)=>{
+router.get("/:name",(request,response)=>{
 const allUsers = getUsers()
-const filterUser = allUsers.filter((use)=> use.id===request.params.id)
+const filterUser = allUsers.filter((use)=> use.name===request.params.name)
 if (filterUser.length>0)
 response.send(filterUser)
 else
 response.send("error")
 
 })
+router.post("/",(request,response)=>{
+    post = { id: uniqId(), ...request.body}
+const allUsers = getUsers()
+allUsers.push(post)
+fs.writeFileSync(usersData , JSON.stringify(allUsers))
+response.send(allUsers)
+})
+
+// router.post("/checkEmail", (request,response)=>{
+// post = request.body
+// const allUsers = getUsers()
+// const use =allUsers.filter(user =>user.id === request.params.id)
+// if(use.length>0){
+//     const user = use.filter(user =>user.email===use.email)
+//     if(user.length>0)
+//     response.send("email cannot be used")
+//     else
+//     response.send("ok ,  email is ok ", user)
+// }else{
+//     response.send("email is empty")
+// }
+
+
+// })
+
+router.put("/:name", (request,response)=>{
+const allUsers = getUsers()
+const filterUsers  = allUsers.filter(user => user.name === request.params.name)
+const User ={ ...request.body,name:request.params.name}
+filterUsers.push(User)
+fs.writeFileSync(usersData,JSON.stringify(allUsers))
+response.send(User)
+
+
+})
+
+router.delete("/:id" , (request,response)=>{
 
 
 
+
+    
+})
 
 
 module.exports = router
