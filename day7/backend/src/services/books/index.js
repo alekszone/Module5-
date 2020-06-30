dfec3253752691d0a202f8679734d27081a42761
alekszone <50597224+alekszone@users.noreply.github.com>
2020-06-30 15:41:34 +0200

    q

--- day7/backend/src/services/books/index.js
+++ day7/backend/src/services/books/index.js
@@ -4,9 +4,11 @@ const { check, validationResult, sanitizeBody } = require("express-validator")
 const fs = require("fs-extra")
 const multer = require("multer")
 const { join } = require("path")
+const uniqid = require("uniqid")
 const { readDB, writeDB } = require("../../utilities")
 
 const booksJsonPath = path.join(__dirname, "books.json")
+const comenntJsonPath = path.join(__dirname,"comment.json")
 
 const booksFolder = join(__dirname, "../../../public/img/books/")
 const upload = multer({})
@@ -38,6 +40,22 @@ booksRouter.get("/:asin", async (req, res, next) => {
   } catch (error) {
     console.log(error)
     next("While reading books list a problem occurred!")
+  }
+})
+booksRouter.get("/:asin/coments", async (req, res, next) => {
+  try {
+    const comments = await readDB(comenntJsonPath)
+    const comm = comments.find((b) => b.bookID === req.params.asin)
+    if (comm.length>0) {
+      res.send(comm)
+    } else {
+      const error = new Error()
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    console.log(error)
+    next("While reading comments list a problem occurred!")
   }
 })
 
@@ -73,6 +91,37 @@ booksRouter.post(
         res.status(201).send("Created")
       }
     } catch (error) {
+      next(error)
+    }
+  }
+)
+booksRouter.post(
+  "/:asin/coments",
+  [
+    check("bookID").exists().withMessage("Book Id "),
+    check("username").exists().isLength({ min: 4 }).withMessage("You should specify the username"),
+    check("text").exists().isLength({ min: 4 }).withMessage("You should ad some text")
+
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      const error = new Error()
+      error.httpStatusCode = 400
+      error.message = errors
+      next(error)
+    }
+    try {
+    
+      const comment = await readDB(comenntJsonPath)
+        const comments = { ...req.body, commentID:uniqid(), date: new Date()}
+      //get a previous element with the same asin
+    
+        comment.push(comments)
+        await writeDB(comenntJsonPath, comment)
+        res.status(201).send("Created comment")
+      }
+     catch (error) {
       next(error)
     }
   }
@@ -117,6 +166,29 @@ booksRouter.delete("/:asin", async (req, res, next) => {
     next(error)
   }
 })
+booksRouter.delete("/coments/:id", async (req, res, next) => {
+  try {
+    const comments = await readDB(comenntJsonPath)
+    const comment = comments.find((b) => b.commentID === req.params.id)
+    if (comment) {
+      await writeDB(
+        comenntJsonPath,
+        comments.filter((x) => x.commentID !== req.params.id)
+      )
+      res.send("Deleted")
+    } else {
+      const error = new Error(`Book with asin ${req.params.asin} not found`)
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+
+
+
 
 booksRouter.post("/upload", upload.single("avatar"), async (req, res, next) => {
   try {
